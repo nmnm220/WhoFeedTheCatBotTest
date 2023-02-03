c3fdffc82fead8a2d0a9dbb6087cd0e14a20ec91
nmnm220 <nmoskalenko@list.ru>
2023-02-03 21:04:33 +0300

    bot token moved to environmental variable

--- src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
+++ src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
@@ -46,7 +46,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return config.getToken();
+        return System.getenv("TOKEN");
     }
 
     @Override
@@ -67,8 +67,8 @@ public class TelegramBot extends TelegramLongPollingBot {
             }
             ArrayList<Food> allFood = whoFedTheCat.listFood();
             List<KeyboardRow> keyboardRows = new ArrayList<>();
-            KeyboardRow row = new KeyboardRow();
             for (Food food: allFood) {
+                KeyboardRow row = new KeyboardRow();
                 row.add(food.brandName());
                 keyboardRows.add(row);
             }
