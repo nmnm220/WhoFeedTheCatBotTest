cfd9845bdda7f11ed2b4c494c6b78291c131f332
nmnm220 <nmoskalenko@list.ru>
2023-02-09 21:00:18 +0300

    refactor class TelegramBot

--- src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
+++ src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
@@ -3,7 +3,6 @@ package io.proj3ct.WhoFeedTheCatBot.service;
 import io.proj3ct.WhoFeedTheCatBot.WhoFedTheCat;
 import io.proj3ct.WhoFeedTheCatBot.WhoFedTheCatDB;
 import io.proj3ct.WhoFeedTheCatBot.config.BotConfig;
-import io.proj3ct.WhoFeedTheCatBot.exceptions.InvalidFoodPriceFormatException;
 import io.proj3ct.WhoFeedTheCatBot.objects.Food;
 import io.proj3ct.WhoFeedTheCatBot.objects.Person;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +20,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -56,90 +54,57 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText() && botState.equals(State.WAITING_FOR_FOOD)) {
+        String messageText = update.getMessage().getText();
+        if (isHasText(update) && botState.equals(State.WAITING_FOR_FOOD)) {
             botState = State.DEFAULT;
             ArrayList<Food> allFood = whoFedTheCat.listFood();
             int foodId = allFood.stream()
                     .filter(food -> update.getMessage().getText().equals(food.brandName()))
                     .toList().get(0).id();
             whoFedTheCat.addCatFeed((int) personId, foodId);
-        }
-        if (update.hasMessage() && update.getMessage().hasText() && botState.equals(State.DEFAULT)) {
-            String messageText = update.getMessage().getText();
-            if (messageText.equals("Stats for all time")) {
-                sendMessage(chatId, whoFedTheCat.getStatsAllTime());
-            } else if (messageText.equals("Stats for today")) {
-                sendMessage(chatId, whoFedTheCat.getStatsWeek());
-            } else if (messageText.equals("Feed cat")) {
-                ArrayList<Person> allPeople = whoFedTheCat.listPeople();
-                personId = allPeople.stream()
-                        .filter(person -> update.getMessage().getFrom().getId() == Long.parseLong(person.telegramId()))
-                        .toList().get(0).id();
-                List<KeyboardRow> keyboardRows = whoFedTheCat.listFood().stream().map(food -> {
-                    KeyboardRow row = new KeyboardRow();
-                    row.add(food.brandName());
-                    return row;
-                }).toList();
-                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-                keyboardMarkup.setResizeKeyboard(true);
-                keyboardMarkup.setKeyboard(keyboardRows);
-                SendMessage message = new SendMessage();
-                message.setChatId(chatId);
-                message.setText("Food");
-                message.setReplyMarkup(keyboardMarkup);
-                try {
-                    execute(message);
-                    botState = State.WAITING_FOR_FOOD;
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
+        } else if (isDefaultAndMessage(update, botState, "/statsalltime")) {
+            sendMessage(chatId, whoFedTheCat.getStatsAllTime());
+        } else if (isDefaultAndMessage(update, botState, "/statstoday")) {
+            sendMessage(chatId, whoFedTheCat.getStatsWeek());
+        } else if (isDefaultAndMessage(update, botState, "/addFood")) {
+            personId = getTelegramIdFromDB(update);
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Food");
+            message.setReplyMarkup(createFoodKeyboardRows());
+            try {
+                execute(message);
+                botState = State.WAITING_FOR_FOOD;
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
             }
-            //sendMessage(chatId, whoFedTheCat.addCatFeed());
-            else if (messageText.startsWith("/addFood")) {
-                String result = update.getMessage().getText().replaceAll("/addFood ", "");
+        }
+        //sendMessage(chatId, whoFedTheCat.addCatFeed());
+        else if (isDefaultAndMessage(update, botState, "/deleteFood")) {
+            String result = update.getMessage().getText().replaceAll("/deleteFood ", "");
+            int id = Integer.parseInt(result);
+            whoFedTheCat.deleteFood(id);
+            sendMessage(chatId, whoFedTheCat.listFood().toString());
+        } else if (isDefaultAndMessage(update, botState, "/addCatFeed")) {
+            String result = update.getMessage().getText().replaceAll("/addCatFeed ", "");
+            String personIdString = result.split(" ")[0];
+            String foodId = result.split(" ")[1];
+            int personIdInt = Integer.parseInt(personIdString);
+            int foodIdInt = Integer.parseInt(foodId);
+            whoFedTheCat.addCatFeed(personIdInt, foodIdInt);
+        } else if (isItCatFeedMessage(update.getMessage().getText())) {
+            long chatId = update.getMessage().getChatId();
+            if (!catFed) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("k:mm");
+                Date currDate = new Date();
+                sendMessage(chatId, update.getMessage().getFrom().getFirstName() + " покормил(а) меня в " + simpleDateFormat.format(currDate) + "." +
+                        "\nУра! Теперь я сыт.");
+                catFed = true;
+                this.chatId = update.getMessage().getChatId();
+            } else {
 
-                //System.out.println(result.split("\\|")[0] + " " + result.split("\\|")[1]);
-                String brandName = result.split(" ")[0];
-                String price = result.split(" ")[1];
-                int priceInt = Integer.parseInt(price);
-                try {
-                    whoFedTheCat.addFood(brandName, priceInt);
-                } catch (InvalidFoodPriceFormatException e) {
-                    throw new RuntimeException(e);
-                }
-                sendMessage(chatId, whoFedTheCat.listFood().toString());
-            } else if (messageText.startsWith("/deleteFood")) {
-                String result = update.getMessage().getText().replaceAll("/deleteFood ", "");
-                int id = Integer.parseInt(result);
-                whoFedTheCat.deleteFood(id);
-                sendMessage(chatId, whoFedTheCat.listFood().toString());
-            } else if (messageText.startsWith("/addCatFeed")) {
-                String result = update.getMessage().getText().replaceAll("/addCatFeed ", "");
-                String personIdString = result.split(" ")[0];
-                String foodId = result.split(" ")[1];
-                int personIdInt = Integer.parseInt(personIdString);
-                int foodIdInt = Integer.parseInt(foodId);
-                whoFedTheCat.addCatFeed(personIdInt, foodIdInt);
-            } else if (messageText.equals("/stats")) {
-                sendMessage(chatId, whoFedTheCat.getStatsAllTime());
-            } else if (messageText.equals("/statsday")) {
-                sendMessage(chatId, whoFedTheCat.getStatsWeek());
-            } else if (parseMessage(update.getMessage().getText())) {
-                long chatId = update.getMessage().getChatId();
-                if (!catFed) {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("k:mm");
-                    Date currDate = new Date();
-                    sendMessage(chatId, update.getMessage().getFrom().getFirstName() + " покормил(а) меня в " + simpleDateFormat.format(currDate) + "." +
-                            "\nУра! Теперь я сыт.");
-                    catFed = true;
-                    this.chatId = update.getMessage().getChatId();
-                } else {
-
-                    //String catNameDec = catName.substring(0, catName.length() - 1) + 'у';
-                    sendMessage(chatId, "Меня уже покормили раньше.");
-                }
-            } else if (messageText.equals("/test")) {
-                sendMessage(chatId, whoFedTheCat.listPeople().toString());
+                //String catNameDec = catName.substring(0, catName.length() - 1) + 'у';
+                sendMessage(chatId, "Меня уже покормили раньше.");
             }
         }
     }
@@ -167,7 +132,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    private boolean parseMessage(String message) {
+    private boolean isItCatFeedMessage(String message) {
         if (message != null) {
             message = message.toLowerCase();
             return message.contains("я покормил");
@@ -182,6 +147,32 @@ public class TelegramBot extends TelegramLongPollingBot {
             String messageToSend = "Я хочу кушать, покормите меня!";
             sendMessage(chatId, messageToSend);
         }
+    }
+
+    private boolean isHasText(Update update) {
+        return update.hasMessage() && update.getMessage().hasText();
+    }
+
+    private boolean isDefaultAndMessage(Update update, State state, String botCommand) {
+        return isHasText(update) && state.equals(State.DEFAULT) && update.getMessage().getText().equals(botCommand);
+    }
+    private int getTelegramIdFromDB (Update update) {
+        ArrayList<Person> allPeople = whoFedTheCat.listPeople();
+        return allPeople.stream()
+                .filter(person -> update.getMessage().getFrom().getId() == Long.parseLong(person.telegramId()))
+                .toList().get(0).id();
+    }
+
+    private ReplyKeyboardMarkup createFoodKeyboardRows() {
+        List<KeyboardRow> keyboardRows = whoFedTheCat.listFood().stream().map(food -> {
+            KeyboardRow row = new KeyboardRow();
+            row.add(food.brandName());
+            return row;
+        }).toList();
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setKeyboard(keyboardRows);
+        return keyboardMarkup;
     }
 
     /*@Scheduled(cron = "0 30 * * * *")
